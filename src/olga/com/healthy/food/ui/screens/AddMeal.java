4e9d956a48963dc249dc9f8b00de9c36d267d027
Marfa Kirina <ola_hnu_86@mail.ru>
2014-10-03 19:02:38 +0300

    create  add meal screen

---
+++ src/olga/com/healthy/food/ui/screens/AddMeal.java
@@ -0,0 +1,15 @@
+package olga.com.healthy.food.ui.screens;
+
+import olga.com.healthy.food.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddMeal extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_meal_screen);
+	}
+}
--- src/olga/com/healthy/food/ui/screens/Menu.java
+++ src/olga/com/healthy/food/ui/screens/Menu.java
@@ -2,7 +2,10 @@ package olga.com.healthy.food.ui.screens;
 
 import olga.com.healthy.food.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class Menu extends Activity {
 
@@ -10,5 +13,17 @@ public class Menu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
+		
+		View changeDate = findViewById(R.id.change_date);
+		
+		View addMealView = findViewById(R.id.add_meal);
+		addMealView.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Menu.this, olga.com.healthy.food.ui.screens.AddMeal.class);
+				Menu.this.startActivity(intent);
+			}
+		});
 	}
 }
